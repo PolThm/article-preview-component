189c6c379c1b1794a5f0028dc602016c3406ba7e
pol.thomas <pol.thomas@niji.fr>
2021-08-22 15:13:13 +0200

    Desktop view done (still need to do the animation)

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
 const toggleSharingBar = () => {
     let sharingBar = document.querySelector('.sharing-bar');
     let authorInfo = document.querySelector('.author-info');
+    const width  = window.innerWidth || document.documentElement.clientWidth ||
+        document.body.clientWidth
 
-    if (sharingBar.classList.contains("active-step-2")) {
-        sharingBar.classList.remove("active-step-2");
-        setTimeout(() => {
-            authorInfo.classList.remove("inactive-step-2");
-            sharingBar.classList.remove("active-step-1");
+    if (width < 576) {
+        if (sharingBar.classList.contains("active-step-2")) {
+            sharingBar.classList.remove("active-step-2");
             setTimeout(() => {
-                authorInfo.classList.remove("inactive-step-1");
-            }, 100)
-        }, 200)
+                authorInfo.classList.remove("inactive-step-2");
+                sharingBar.classList.remove("active-step-1");
+                setTimeout(() => {
+                    authorInfo.classList.remove("inactive-step-1");
+                }, 100)
+            }, 200)
+        } else {
+            authorInfo.classList.add("inactive-step-1");
+            setTimeout(() => {
+                sharingBar.classList.add("active-step-1");
+                authorInfo.classList.add("inactive-step-2");
+                setTimeout(() => {
+                    sharingBar.classList.add("active-step-2");
+                }, 100)
+            }, 200)
+        }
     } else {
-        authorInfo.classList.add("inactive-step-1");
-        setTimeout(() => {
-            sharingBar.classList.add("active-step-1");
-            authorInfo.classList.add("inactive-step-2");
-            setTimeout(() => {
-                sharingBar.classList.add("active-step-2");
-            }, 100)
-        }, 200)
-
+        if (sharingBar.classList.contains("active-step-2")) {
+            sharingBar.classList.remove("active-step-1", "active-step-2");
+            authorInfo.classList.remove("inactive-step-1", "inactive-step-2");
+        } else {
+            sharingBar.classList.add("active-step-1", "active-step-2");
+            authorInfo.classList.add("inactive-step-1", "inactive-step-2");
+        }
     }
 }
